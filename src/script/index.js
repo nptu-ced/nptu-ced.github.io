f07de4516ff2bed9b02a4fc6a8ce7a1e8d69609c
karta2341940 <a08487a@gmail.com>
2022-12-08 16:13:55 +0800

    finish

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,21 @@
-import {createApp} from "vue"
-import layout from "./layout/layout.vue";
-createApp(layout).mount("#app");
+let index = 1;
+let stringURL = `src/book/英文-${index}.xhtml`
+let iframe = document.querySelector("iframe");
+let right = document.querySelector(".right");
+let left = document.querySelector(".left");
+right.addEventListener("click",e=>{
+    if(index == 53) return;
+    index++;
+    stringURL = `src/book/英文-${index}.xhtml`
+    console.log(stringURL)
+    iframe.src = stringURL
+    console.log(iframe.src)
+})
+left.addEventListener("click",e=>{
+    if(index == 1) return;
+    index--;
+    stringURL = `src/book/英文-${index}.xhtml`
+    console.log(stringURL)
+    iframe.src = stringURL
+    console.log(iframe.src)
+})
