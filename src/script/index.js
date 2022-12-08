992ed11d8cf139d63b0d88775f29a39b97260a54
karta2341940 <a08487a@gmail.com>
2022-12-08 15:33:57 +0800

    update

---
+++ src/script/index.js
@@ -0,0 +1,3 @@
+import {createApp} from "vue"
+import layout from "./layout/layout.vue";
+createApp(layout).mount("#app");
---
+++ src/script/layout/layout.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    data() {
+        return {
+            data: "Hello"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+    position: absolute;
+    z-index: 99;
+    width: 35vw;
+    height: 99vh;
+}
+
+.left {
+    left: 0px;
+}
+
+.right {
+    right: 0px;
+}
+.content{
+    z-index: 1;
+}
+iframe{
+    width: 50vw;
+    height: 50vh;
+}
+</style>
+
+<template>
+    <div class="left btn"></div>
+    <div class="content">
+        {{data}}
+    </div>
+    <div class="right btn"></div>
+</template>
